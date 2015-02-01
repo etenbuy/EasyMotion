8d3a1f6088a93eaed19ae28ff9f2a86ce2b38d2f
ftvoid <gensaicuttlefish@gmail.com>
2015-02-01 12:33:13 +0900

    直線モーション実装。

---
+++ Assets/Motion2D/Scripts/LinerMotion.cs
@@ -0,0 +1,46 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                               //
+//  File    :   LinerMotion.cs                                                                   //
+//  Author  :   ftvoid                                                                           //
+//  Date    :   2015.02.01                                                                       //
+//  Desc    :   直線モーション。                                                                 //
+//                                                                                               //
+///////////////////////////////////////////////////////////////////////////////////////////////////
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 直線モーション
+/// </summary>
+public class LinerMotion : MotionBase2D {
+    /// <summary>
+    /// 始点
+    /// </summary>
+    [SerializeField]
+    private Vector2 from = Vector2.zero;
+
+    /// <summary>
+    /// 終点
+    /// </summary>
+    [SerializeField]
+    private Vector2 to = Vector2.zero;
+
+    /// <summary>
+    /// 移動開始までの時間
+    /// </summary>
+    [SerializeField]
+    private float delay = 0;
+
+    /// <summary>
+    /// 移動時間
+    /// </summary>
+    [SerializeField]
+    private float duration = 0;
+
+    /// <summary>
+    /// 直線移動コルーチンを実行する
+    /// </summary>
+    private void Start() {
+        StartCoroutine(Line(from, to, delay, duration));
+    }
+}
