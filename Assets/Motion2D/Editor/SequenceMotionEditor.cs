80487b023ef53f8d06da5eb5151a7225cdb3f08f
ftvoid <gensaicuttlefish@gmail.com>
2015-02-04 22:42:32 +0900

    連続モーションのエディタ拡張作成途中。

--- Assets/Motion2D/Editor/SequenceMotionEditor.cs
+++ Assets/Motion2D/Editor/SequenceMotionEditor.cs
@@ -21,12 +21,42 @@ public class SequenceMotionEditor : Editor {
     /// MotionSequenceのインスペクタ上のレイアウト
     /// </summary>
     public override void OnInspectorGUI() {
-        DrawDefaultInspector();
+        //DrawDefaultInspector();
 
         serializedObject.Update();
 
-        // TODO 配列UIを動的表示する
+        // 配列UIを動的表示する
+        var sequence = serializedObject.FindProperty("sequence");
+        var arraySize = sequence.arraySize;
+
+        for ( int i = 0 ; i < arraySize ; ++i ) {
+            //EditorGUILayout.PropertyField(sequence.GetArrayElementAtIndex(i), true);
+
+            var elem = sequence.GetArrayElementAtIndex(i);
+
+            switch ( (MotionSequence.MotionType)elem.FindPropertyRelative("type").enumValueIndex ) {
+            case MotionSequence.MotionType.Line:
+                DrawLineGUI(elem);
+                break;
+
+            default:
+                EditorGUILayout.PropertyField(elem, true);
+                break;
+            }
+        }
 
         serializedObject.ApplyModifiedProperties();
+    }
+
+    /// <summary>
+    /// 直線移動のGUI描画
+    /// </summary>
+    /// <param name="prop"></param>
+    private void DrawLineGUI(SerializedProperty prop) {
+        EditorGUILayout.LabelField("Element");
+        EditorGUILayout.PropertyField(prop.FindPropertyRelative("type"));
+        EditorGUILayout.PropertyField(prop.FindPropertyRelative("delay"));
+        EditorGUILayout.PropertyField(prop.FindPropertyRelative("duration"));
+        EditorGUILayout.PropertyField(prop.FindPropertyRelative("from"));
     }
 }
