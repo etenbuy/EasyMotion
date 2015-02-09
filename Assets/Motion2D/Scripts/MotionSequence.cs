7f72ab8552c4f4ab3899c0dd0df5cc448b3a8088
ftvoid <gensaicuttlefish@gmail.com>
2015-02-10 00:37:05 +0900

    MotionSequenceの一連の動作実装。

--- Assets/Motion2D/Scripts/MotionSequence.cs
+++ Assets/Motion2D/Scripts/MotionSequence.cs
@@ -85,5 +85,32 @@ public class MotionSequence : MotionBase2D {
     /// 初期化
     /// </summary>
     private void Start() {
+        StartCoroutine(ExecuteMotion());
+    }
+
+    /// <summary>
+    /// モーションシーケンスを実行する
+    /// </summary>
+    private IEnumerator ExecuteMotion() {
+        foreach ( var motion in sequence ) {
+            switch ( motion.type ) {
+            case MotionType.Line:
+                // 直線移動
+                Debug.Log("MotionType.Line start");
+                yield return StartCoroutine(Line(motion.from, motion.to, motion.delay, motion.duration));
+                Debug.Log("MotionType.Line end");
+                break;
+
+            case MotionType.Curve:
+                // 旋回移動
+                yield return StartCoroutine(Curve(motion.from, motion.fromAngle, motion.rotateAngle, motion.radius, motion.delay, motion.duration));
+                break;
+
+            default:
+                // 静止
+                yield return new WaitForSeconds(motion.delay + motion.duration);
+                break;
+            }
+        }
     }
 }
